e06a0564da2b23744a42a3bf8e3710dfd0cce585
Your Name <you@example.com>
2026-03-09 22:13:23 +0900

    trying to make a new class bc IR class is whack. I didn't know about rect in pygame being stupid and the pygame-ce docs for frect for example being fucking retarded and made by absolute fucking retard pieces of fucking dogshit

--- 24/src/main.py
+++ 24/src/main.py
@@ -1,15 +1,21 @@
 
 import pygame
+import global_vars
+keys = global_vars.keys
 from funcs import init, pe
 from Image_Rect import Image_Rect
 IR = Image_Rect
 
 screen, clock = init(1760, 1024)
 
-m = IR("data/m_03_02.png", 10, 10)
-m.speed = 0.3
-background = IR("data/background.png", 0, 0)
+class Player:
+    def __init__(self, x: float, y: float):
+        self.x = x
+        self.y = y
 
+player = Player(10.0, 10.0)
+m_img: pygame.Surface = pygame.image.load("data/m_03_02.png")
+background = IR("data/background.png", 0, 0)
 rock = IR("data/rock 01.png", 64, 64)
 
 fb = pygame.Surface((256, 160))
@@ -19,13 +25,12 @@ while True:
     pe()
 
     # process input
-    #m.x += keys["right"] - keys["left"]
-    #m.y += keys["down"] - keys["up"]
+    player.x += float(keys["right"] - keys["left"])
+    player.y += float(keys["down"] - keys["up"])
 
     # draw to fb
-    fb.fill((14, 255, 255))
     background.draw(fb)
-    m.draw(fb)
+    fb.blit(m_img, (player.x, player.y))
     rock.draw(fb)
 
     # handle screen
