0aefe8d935b55b72461411079db4b02bd44078a2
Your Name <you@example.com>
2026-03-09 20:39:02 +0900

    player moves around. changes to l01 bc float/int issues

--- 24/src/main.py
+++ 24/src/main.py
@@ -3,12 +3,15 @@ import pygame
 from l01 import *
 keys = global_vars.keys
 
-screen, clock = init(1920, 1280)
+screen, clock = init(1760, 1024)
 
 m = IR("data/m_03_02.png", 10, 10)
-background = IR("data/desert_scene_01.png", 0, 0)
+m.speed = 0.3
+background = IR("data/background.png", 0, 0)
 
-fb = pygame.Surface((192, 128))
+rock = IR("data/rock 01.png", 64, 64)
+
+fb = pygame.Surface((256, 160))
 
 while True:
     # get input
@@ -21,8 +24,10 @@ while True:
     # draw to fb
     fb.fill((14, 255, 255))
     background.draw(fb)
-    fb.blit(m.img, m.pos)
+    m.draw(fb)
+    rock.draw(fb)
 
     # handle screen
     screen.blit(pygame.transform.scale(fb, screen.get_size()), (0,0))
+    clock.tick(60)
     pygame.display.update()
